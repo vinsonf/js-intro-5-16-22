24adf7185e5ec4ed93364f3626a98b0f582fe923
vinferno <vinsonfernandez@gmail.com>
2022-05-25 21:03:01 -0700

    challenge

--- day03/js/main.js
+++ day03/js/main.js
@@ -2,26 +2,52 @@ const title = 'My page!';
 const value = '1,0,2';
 let answer = value.split(',');
 
-
-
-
-
-
 console.log(`101010101`.replaceAll('0', 'x'));
 
 
-const myAlert = alert('Welcome to my page!');
+// const myAlert = alert('Welcome to my page!');
 
-const myConfirm = confirm('Are you sure?');
+// const myConfirm = confirm('Are you sure?');
 
-const myPrompt = prompt('What is your name?');
+// const myPrompt = prompt('What is your name?');
 
-console.log(myAlert, myConfirm, myPrompt);
+// console.log(myAlert, myConfirm, myPrompt);
 
-answer = myPrompt;
+// answer = myPrompt;
 
 document.querySelector('body').innerHTML = `<h1>Hello, ${answer}</h1>`; // this is putting the answer into the html
 
 
 
 console.log(answer); // this is logging the answer to the console
+
+
+console.log(Math);
+
+console.log(Math.abs(-5));
+
+console.log(Math.acos(0.5));
+
+console.dir(new Date())
+
+
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+
+setInterval(() => {
+    answer = new Date();
+    document.querySelector('body').innerHTML = `<h1>This is the date information<h1>
+    <p>Month: ${months[answer.getMonth()]}</p>
+    <p>Day: ${answer.getDate()}</p>
+    <p>Year: ${answer.getFullYear()}</p>
+    <p>Hours: ${answer.getHours() % 12}</p>
+    <p>Minutes: ${answer.getMinutes()}</p>
+    <p>Seconds: ${answer.getSeconds()}</p>
+    <p>${ String(answer.getHours() % 12).toString().padStart(2, '0') } : ${answer.getMinutes().toString().padStart(2, '0')} : ${answer.getSeconds().toString().padStart(2, '0')}</p>
+    `;
+    console.log('running..')
+}, 1000);
+
+
+
---
+++ day04/js/review.js
@@ -0,0 +1,55 @@
+let myString = 'Hello World'; // let is a variable that can change
+const myAppName = 'My App'; // const is a variable that cannot change
+
+
+// reserved keywords
+// const, let, var, function, this, class, 
+
+
+console.log('string', "string", `string${myString}`);
+console.log(1, 2.7);
+console.log(true, false);
+
+// memory = {  variableName: data   }
+
+console.log(myString);
+
+
+let myNumber = 5; 
+console.log( myNumber  + 5);
+
+
+
+
+console.log( myNumber += 5);
+
+console.log('*', myNumber += '5', '*');
+
+console.log(myNumber - 100);
+
+score = 0;
+
+myTitle = 'Very cool app';
+
+// coercion // dynamic typing
+
+console.log( 20 % 3);
+
+console.log('Hello'.toString())
+
+// 3 types of popups
+// alert, confirm, prompt
+
+// alert('This is a message'); // return undefined
+
+// confirm('Are you sure?'); // return true or false
+
+// prompt('What is your name?', 'Player 1'); // return string
+
+
+Math.pow(10)
+
+// alert();
+// const day = prompt('what date');
+// const myDay = new Date(day);
+// console.log(myDay);
